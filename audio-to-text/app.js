7ba9cd911de967bf31ad5f59697be881e8f3de34
Winder Díaz <winderdiaz29@gmail.com>
2023-06-08 08:40:34 -0400

    audiototext done

--- audio-to-text/app.js
+++ audio-to-text/app.js
@@ -1,74 +1,95 @@
 const msgEl = document.getElementById('msg');
+const listen = document.querySelector('#listen');
+const stopBtn = document.querySelector('#stop');
+
 
 const randomNum = getRandomNumber();
-
-console.log('Number:', randomNum);
-
-window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-
-let recognition = new window.SpeechRecognition();
-recognition.lang = 'es-ES'
-
-// Start recognition and game
-recognition.start();
-
-// Capture user speak
-function onSpeak(e) {
-  const msg = e.results[0][0].transcript;
-
-  writeMessage(msg);
-  checkNumber(msg);
-}
-
-// Write what user speaks
-function writeMessage(msg) {
-  msgEl.innerHTML = `
-    <div>Dijiste: </div>
-    <span class="box">${msg}</span>
-  `;
-}
-
-// Check msg against number
-function checkNumber(msg) {
-  const num = +msg;
-
-  // Check if valid number
-  if (Number.isNaN(num)) {
-    msgEl.innerHTML += '<div>Ese no es un numero válido</div>';
-    return;
-  }
-
-  // Check in range
-  if (num > 100 || num < 1) {
-    msgEl.innerHTML += '<div>El numero debe ser entre 1 y 100</div>';
-    return;
-  }
-
-  // Check number
-  if (num === randomNum) {
-    document.body.innerHTML = `
-      <h2>Felicidades, has adivinado <br><br>
-      era el ${num}</h2>
-      <button class="play-again" id="play-again">Jugar de nuevo</button>
-    `;
-  } else if (num > randomNum) {
-    msgEl.innerHTML += '<div>Intenta con uno más bajo</div>';
-  } else {
-    msgEl.innerHTML += '<div>Intenta con uno más alto</div>';
-  }
-}
 
 // Generate random number
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-// Speak result
+console.log('Number:', randomNum);
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+recognition.lang = 'es-ES' // Definimos el idioma a procesar
+
+let listening; // Bandera para saber si está escuchando
+
+listen.addEventListener('click', () => {
+  // Comenzar reconocimiento
+  recognition.start();
+  listening = true
+  console.log('started')
+})
+stopBtn.addEventListener('click', () => {
+  // Detener el reconocimiento
+  recognition.stop();
+  listening = false
+  console.log('aborted')
+  msgEl.innerHTML += '<div>Vuelve a iniciar el servicio para escuchar</div>';
+})
+
+// // Escuchamos el evento result que es lo obtenido del reconocimiento
 recognition.addEventListener('result', onSpeak);
 
-// End SR service
+// // Terminamos la captura del audio e iniciamos el procesamiento del audio
 recognition.addEventListener('end', () => recognition.start());
+
+// Capturar el audio del usuario
+function onSpeak(evt) {
+  const msg = evt.results[0][0].transcript;
+  console.log(msg)
+
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Escribir el mensaje
+function writeMessage(msg) {
+  if (listening) {
+    msgEl.innerHTML = `
+      <div>Dijiste: </div>
+      <span class="box">${msg}</span>
+    `
+  }
+}
+
+// Comparar mensaje con un numero
+function checkNumber(msg) {
+  const num = +msg;
+
+  if (listening) {
+    // Valida si es un nro valido
+    if (Number.isNaN(num) && listening) {
+      msgEl.innerHTML += '<div>Ese no es un numero válido</div>';
+      return;
+    }
+  
+    // Valida el rango
+    if (num > 100 || num < 1) {
+      msgEl.innerHTML += '<div>El numero debe ser entre 1 y 100</div>';
+      return;
+    }
+  
+    // Compara con el nro a adivinar
+    if (num === randomNum) {
+      document.body.innerHTML = `
+        <h2>Felicidades, has adivinado <br><br>
+        era el ${num}</h2>
+        <button class="play-again" id="play-again">Jugar de nuevo</button>
+      `;
+    } else if (num > randomNum) {
+      msgEl.innerHTML += '<div>Intenta con uno más bajo</div>';
+    } else {
+      msgEl.innerHTML += '<div>Intenta con uno más alto</div>';
+    }
+  }
+
+}
 
 document.body.addEventListener('click', e => {
   if (e.target.id == 'play-again') {
